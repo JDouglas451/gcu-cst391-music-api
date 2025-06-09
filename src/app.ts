621b960e868dfb95ffb31c773db6446568d13b35
Jdouglas451 <jgjpdouglas@gmail.com>
2025-06-08 20:36:17 -0400

    Typescript hello world app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+// register root page
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World from TypeScript!");
+});
+
+// start the app
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
